a078692ee273f40e0c4c5820e5d6e81badd5e018
Jesus <Jesus@10.0.2.15>
2018-03-11 17:36:07 -0600

    Se cambio el nombre de la aplicacion

--- backend/config/main.php
+++ backend/config/main.php
@@ -41,12 +41,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        /*  
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-            ], prueba mse
+            ], prueba 2
         ],
         */
     ],

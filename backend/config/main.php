2c0413ceca84a293023fe584be4de4ff82d2d54e
Jesus <Jesus@10.0.2.15>
2018-03-11 13:52:02 -0600

    Creación del repositorio

--- backend/config/main.php
+++ backend/config/main.php
@@ -46,7 +46,7 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-            ],
+            ], prueba mse
         ],
         */
     ],

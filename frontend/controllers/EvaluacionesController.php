2197c0be05a599a858f312e99cacb675d911ffc9
Jesus <Jesus@10.0.2.15>
2018-03-29 11:05:23 -0600

    Revert "Consulta de evaluaciones a pata"
    This reverts commit d6a8dbdfe70092f31a9283be03906c182e270ea6.

--- frontend/controllers/EvaluacionesController.php
+++ frontend/controllers/EvaluacionesController.php
@@ -10,37 +10,16 @@ class EvaluacionesController extends Controller
 {
     
     
-    public function actionDominios()
+    public function actionIndex()
     {
     
        $sql=( new \yii\db\Query())->select('*')->from('dominios')->All();
     
-       return $this->render('dominios', array('items'=>$sql));
+       return $this->render('index', array('items'=>$sql));
 }
 
-public function actionControles($idEvaluacion, $idDominio) {
-
-        //$sql = ( new \yii\db\Query())->select('*')->from('controles')->where(['Id_Dominio' => [$idDominio]]);
-
-        $sql = (new \yii\db\Query())
-                ->select('*')
-                ->from('controles')
-                ->where(['Id_Dominio' => $idDominio])
-                ->all();
-
-        return $this->render('controles', array('items' => $sql));
-        //$model = new \common\models\Controles();
-        //return $this->render('controles', ['id' =>$idEvaluacion, 'idDominio' => $idDominio,]);
-        // return $this->render('controles', ['model' => $this->findModel($idDominio), ]);
-        //return $this->redirect(array('evaluar', 'id' => $id, 'idDominio' => $idDominio));
     }
 
-    protected function findModel($idDominio)
-    {
-        if (($model = \common\models\Controles::findOne($idDominio)) !== null) {
-            return $model;
-        }
-        throw new NotFoundHttpException('The requested page does not exist.');
-    }
-}
+
+
 ?>

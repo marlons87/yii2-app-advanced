963477537bffa5f777080e4dc95e537bffe46327
Jesus <Jesus@10.0.2.15>
2018-03-29 14:18:58 -0600

    Prueba

--- frontend/views/evaluaciones/index.php
+++ frontend/views/evaluaciones/index.php
@@ -3,9 +3,69 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 
-foreach ($items as $i):?>
-<br><?php echo $i['Nombre'] ; ?>
-   <?php endforeach;?>
+$this->title = 'EMC2 | Evaluaciones';
+?>
+<h1>Modelo de Madurez en Ciberseguridad</h1>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Mis Evaluaciones</h3>
+  </div>
+  <div class="panel-body">
+  
+       
+  <?php
+  
+  
+
+ 
+ if (sizeof($items)==0){
+     
+      ?>
+      
+      <p>El usuario no posee evaluaciones</p>
+       <?php
+ }else{
+   
+ foreach ($items as $i):
+          ?>
+          
+          <div class="panel panel-default">
+      <div class="panel-heading">
+          <h3 class="panel-title">Evaluaci&oacute;n <?php echo $i['Consecutivo']; ?></h3>
+      </div>
+      <div class="panel-body">
+          <p>Fecha creaci&oacute;n: <?php echo $i['Fecha']; ?></p>
+    <?php if ($i['Status']) {
+        ?> 
+                     <p>Estado: Completa</p>
+        <?php
+    } else {
+        ?>  
+              <p>Estado: Incompleta</p>
+              <p>&Uacute;ltima modificaci&oacute;n:<?php echo $i['Fecha_Ultima_Modificacion']; ?></p>
+              
+        <?= Html::a('Ver detalles', ['update', 'id' => $i['Id_Evaluacion']], ['class' => 'btn btn-primary']) ?>
+        <?php
+    }
+    ?>
+          
+
+                </div>
+    </div>
+        
+          
+<?php endforeach; 
+ }
+ 
+?> 
+
+      
+      
+  </div>
+</div>
 
 
-    
+ <p>
+        <?= Html::a('Crear evaluacion', ['dominios'], ['class' => 'btn btn-success']) ?>
+    </p>

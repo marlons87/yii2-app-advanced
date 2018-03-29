d6a8dbdfe70092f31a9283be03906c182e270ea6
Jesus <Jesus@10.0.2.15>
2018-03-29 10:56:35 -0600

    Consulta de evaluaciones a pata

--- frontend/controllers/EvaluacionesController.php
+++ frontend/controllers/EvaluacionesController.php
@@ -10,16 +10,37 @@ class EvaluacionesController extends Controller
 {
     
     
-    public function actionIndex()
+    public function actionDominios()
     {
     
        $sql=( new \yii\db\Query())->select('*')->from('dominios')->All();
     
-       return $this->render('index', array('items'=>$sql));
+       return $this->render('dominios', array('items'=>$sql));
 }
 
+public function actionControles($idEvaluacion, $idDominio) {
+
+        //$sql = ( new \yii\db\Query())->select('*')->from('controles')->where(['Id_Dominio' => [$idDominio]]);
+
+        $sql = (new \yii\db\Query())
+                ->select('*')
+                ->from('controles')
+                ->where(['Id_Dominio' => $idDominio])
+                ->all();
+
+        return $this->render('controles', array('items' => $sql));
+        //$model = new \common\models\Controles();
+        //return $this->render('controles', ['id' =>$idEvaluacion, 'idDominio' => $idDominio,]);
+        // return $this->render('controles', ['model' => $this->findModel($idDominio), ]);
+        //return $this->redirect(array('evaluar', 'id' => $id, 'idDominio' => $idDominio));
     }
 
-
-
+    protected function findModel($idDominio)
+    {
+        if (($model = \common\models\Controles::findOne($idDominio)) !== null) {
+            return $model;
+        }
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}
 ?>

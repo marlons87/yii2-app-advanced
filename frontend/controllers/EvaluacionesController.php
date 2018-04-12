8e112c40f098abf7e10ede98051b5a55ed044678
Jesus <Jesus@10.0.2.15>
2018-04-11 21:54:39 -0600

    Evaluacion con respuestas desde la base de datos

--- frontend/controllers/EvaluacionesController.php
+++ frontend/controllers/EvaluacionesController.php
@@ -10,6 +10,8 @@ use common\models\Niveles;
 use common\models\ControlesSearch;
 use common\models\Respuestas;
 use common\models\Evaluaciones;
+use frontend\models\EvaluacionForm;
+use yii\db\Schema;
 
 class EvaluacionesController extends Controller {
 
@@ -34,31 +36,23 @@ class EvaluacionesController extends Controller {
 
         throw new NotFoundHttpException('The requested page does not exist.');
     }
-    
-    
-     public function actionDominios($id) {
-         
-            $sql = ( new \yii\db\Query())->select('*')->from('dominios')->All();
-            
-            $calificacion = Yii::$app->db->createCommand('select dominios.Id_Dominio, dominios.Codigo as DominioCodigo, dominios.Nombre as DominioNombre, controles.Id_Control, controles.Codigo, controles.Nombre, niveles.Valor
-from dominios
-LEFT join controles on dominios.Id_Dominio = controles.Id_Dominio
-left join niveles on controles.Id_Control = niveles.Id_Control
-left join respuestas on controles.Id_Control = respuestas.Id_Control and niveles.Id_Nivel = respuestas.Id_Nivel
-where (respuestas.Id_Evaluacion is null OR respuestas.Id_Evaluacion = :id) and respuestas.Id_Nivel IS NOT null
-order by dominios.Id_Dominio, controles.Id_Control, niveles.Id_Nivel ASC')
+
+    public function actionDominios($id) {
+
+        $sql = ( new \yii\db\Query())->select('*')->from('dominios')->All();
+
+        $calificacion = Yii::$app->db->createCommand('select dominios.Id_Dominio, dominios.Codigo as DominioCodigo, dominios.Nombre as DominioNombre, controles.Id_Control, controles.Codigo, controles.Nombre, niveles.Valor
+        from dominios
+        LEFT join controles on dominios.Id_Dominio = controles.Id_Dominio
+        left join niveles on controles.Id_Control = niveles.Id_Control
+        left join respuestas on controles.Id_Control = respuestas.Id_Control and niveles.Id_Nivel = respuestas.Id_Nivel
+        where (respuestas.Id_Evaluacion is null OR respuestas.Id_Evaluacion = :id) and respuestas.Id_Nivel IS NOT null
+        order by dominios.Id_Dominio, controles.Id_Control, niveles.Id_Nivel ASC')
                 ->bindValue(':id', $id)
                 ->queryAll();
-            
-            
-            
-            
 
-      return $this->render('dominios', array('items' => $sql,'calificacion'=>$calificacion,'id'=>$id));
-         
-     }
-    
-    
+        return $this->render('dominios', array('items' => $sql, 'calificacion' => $calificacion, 'id' => $id));
+    }
 
     public function actionInsertar() {
 
@@ -73,7 +67,7 @@ order by dominios.Id_Dominio, controles.Id_Control, niveles.Id_Nivel ASC')
             'Id_Usuario' => yii::$app->user->identity->Id,
             'Id_Institucion' => yii::$app->user->identity->Id_Institucion,
             'Fecha_Ultima_Modificacion' => date('Y-m-d H:i:s'),])->execute();
-        
+
 
         $evaluacion = Yii::$app->db->createCommand('SELECT LAST_INSERT_ID() as evaluacion')
                 ->queryOne();
@@ -83,8 +77,6 @@ order by dominios.Id_Dominio, controles.Id_Control, niveles.Id_Nivel ASC')
 
         return $this->render('evaluar', array('items' => $sql, 'evaluacion' => $evaluacion));
     }
-    
-    
 
     public function actionControles($idEvaluacion, $idDominio) {
         $sql = (new \yii\db\Query())
@@ -98,15 +90,78 @@ order by dominios.Id_Dominio, controles.Id_Control, niveles.Id_Nivel ASC')
         return $this->render('controles', array('items' => $sql));
     }
     
-    public function actionPruebas($idEvaluacion, $idDominio) {
+       public function actionPruebas($idEvaluacion, $idDominio) {
         $dominios = Dominios::findOne($idDominio);
         $controles = $dominios->controles;
         $evaluaciones = Evaluaciones::findOne($idEvaluacion);
         $respuestas = $evaluaciones->respuestas;
+        return $this->render('pruebas', array('controles' => $controles, 'respuestas' => $respuestas, 'idevaluacion'=>$idEvaluacion, 'iddominio'=>$idDominio));
+   }
+       
+       
+       public function actionPruebas2($idEvaluacion, $idDominio) {
+        $evaluacion = Evaluaciones::findOne($idEvaluacion);
+        $respuestas = $evaluacion->respuestas;
+        $control = $respuestas->control;
+        $niveles = $control->niveles;
         //var_dump($respuestas);
-//Se deben incluier los Niveles de cada control y determinar cual esta saleccionado por el usuario si ya existe una evaluacion
-        return $this->render('pruebas', array('controles' => $controles, 'respuestas' => $respuestas));
-    
+        //Se deben incluier los Niveles de cada control y determinar cual esta saleccionado por el usuario si ya existe una evaluacion
+        return $this->render('pruebas2', array('respuestas' => $respuestas));
+        
+       }
+       
+        //Controller file code
+    public function actionAjax()
+        {
+            $data = Yii::$app->request->post('niveles');
+            $observaciones = Yii::$app->request->post('observaciones');
+            $idEvaluacion = Yii::$app->request->post('idEvaluacion');
+            if (isset($data)) {
+                foreach ($data as $respuesta)
+                {
+                    $nivel = Niveles::findOne($respuesta);
+                    $control = $nivel['Id_Control'];
+                    //Asignar el comentario de cada control
+                    $comentario='mmm';
+                    foreach ($observaciones as $observacion)
+                    {
+                        $myArray = explode('~', $observacion);
+                        if ($myArray[0] === $control) {
+                            $comentario = $myArray[1];
+                        }
+                    }
+                    
+                    $respuestaactual = Respuestas::find()
+                            ->where(['Id_Evaluacion'=>$idEvaluacion])
+                            ->andWhere(['Id_Control'=>$control])
+                            ->andWhere(['Id_Nivel'=>$respuesta])
+                            ->one();
+                    if(!$respuestaactual['Id_Respuesta'])
+                    {
+                        $respuestaactual = Respuestas::find()
+                            ->where(['Id_Evaluacion'=>$idEvaluacion])
+                            ->andWhere(['Id_Control'=>$control])
+                            ->one(); 
+                        if(!$respuestaactual['Id_Respuesta'])
+                    {  //Inserta uno nuevo
+                            $respuestanueva = new Respuestas();
+                           $respuestanueva->Id_Nivel=$respuesta;
+                           $respuestanueva->Id_Evaluacion=$idEvaluacion;
+                           $respuestanueva->Id_Control=$control;
+                           $respuestanueva->Observaciones=$comentario;
+                           $respuestanueva->save();
+                    }     
+                    else{//Actualizar
+                        $respuestaactual->Id_Nivel= intval($respuesta);
+                        $respuestaactual->Observaciones = $comentario;
+                        $respuestaactual->save();
+                    }   }                   
+                }
+                
+            } else {
+                $test = "Ajax failed";
+            }
+            return \yii\helpers\Json::encode($test);
+        }
 
-}
-}
+}        

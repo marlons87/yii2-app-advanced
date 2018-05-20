0f853ff2ce059faff28cf54fb4deb7110c84aebf
Jesus <Jesus@10.0.2.15>
2018-05-19 19:58:23 -0600

    se continua con la retroalimentacion proporcionada por Cesar

--- frontend/controllers/EvaluacionesController.php
+++ frontend/controllers/EvaluacionesController.php
@@ -60,8 +60,7 @@ class EvaluacionesController extends Controller {
                 ->bindValue(':id', $id)
                 ->queryAll();
 
-        $evaluacion = Yii::$app->db->createCommand('SELECT Id_Evaluacion,Consecutivo,descripcion,instituciones.Nombre,evaluaciones.Fecha,evaluaciones.Fecha_Ultima_Modificacion
-FROM evaluaciones INNER JOIN instituciones ON evaluaciones.Id_Institucion=instituciones.Id_Institucion and evaluaciones.Id_Evaluacion=:id')
+        $evaluacion = Yii::$app->db->createCommand('SELECT e.Id_Evaluacion, Consecutivo, descripcion, i.Nombre, e.Fecha, e.Fecha_Ultima_Modificacion FROM evaluaciones e INNER JOIN sedes s on e.Id_Sede = s.Id_Sede INNER JOIN instituciones i ON s.Id_Institucion = i.Id_Institucion and e.Id_Evaluacion=:id')
                 ->bindValue(':id', $id)
                 ->queryOne();
 
